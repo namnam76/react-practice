332941176420bce69b9a289c31c5daaa5b935b61
namnam76 <53989471+namnam76@users.noreply.github.com>
2021-06-26 16:28:32 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    // タグをまとめるには<React.Fragment>か<>
+    <React.Fragment>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </React.Fragment>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
