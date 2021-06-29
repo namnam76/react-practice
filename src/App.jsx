4851cd56960f5cab6439f54aaca10a1aebfd003a
namnam76 <53989471+namnam76@users.noreply.github.com>
2021-06-29 15:25:28 +0900

    add ReactCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
-
+/* eslint react-hooks/exhaustive-deps:off */
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  useEffect(() => {
+    console.log("useEffect!");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     // タグをまとめるには<React.Fragment>か<>
     <React.Fragment>
@@ -10,7 +29,11 @@ const App = () => {
       {/* <ColorfulMessage color="pink" message="元気です！" />  */}
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( *´艸｀)</p>}
     </React.Fragment>
   );
 };
